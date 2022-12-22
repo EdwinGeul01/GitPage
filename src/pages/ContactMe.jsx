827860cde1646b6fcebe01f7bb4411b609991595
Edwin Geul <edwingeul01@gmail.com>
2022-12-22 02:13:39 -0600

    gh-pages added

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -6,7 +6,7 @@ export default function ContactMe() {
     <div className=' w-full h-[calc(100vh-100px)] mt-3 px-3 py-4 flex flex-col  items-center'>
       <div className='w-[80%] '>
         <h1 className="text-4xl BebasFont mb-10 text-white">CONTACT ME</h1>
-        <div className='w-full lg:px-20 border border-double border-[5px] py-10 flex flex-col text-white'>
+        <div className='w-full lg:px-20  border-double border-[5px] py-10 flex flex-col text-white'>
         <span >
           <p className='mb-10'>
             <Phone/>  <WhatsApp />  +504 3239-2413 <br />
